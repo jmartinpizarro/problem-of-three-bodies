2be5e24818bad83d7a0debd8b9398070ec43afa5
Javier Martin Pizarro <jmartinpizarro04@gmail.com>
2025-02-08 11:17:44 +0100

    add more data structures

--- body.h
+++ body.h
@@ -11,4 +11,21 @@ struct body {
   double ax, ay;
 };
 
+struct node { // used for saving a pair of positions
+  int px;
+  int py;
+};
+
+struct path {
+  node* previous_positions;// will save the last ten positions of an orbital body
+
+  path() {
+    previous_positions = new node[10]; // save memory for 10 positions
+  }
+
+  ~path() {
+    delete[] previous_positions; // free memory automatically
+  }
+};
+
 #endif // BODY_H
